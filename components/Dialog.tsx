31c73fb0a94b9f8bb312d0d957988dc276656207
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-09-01 19:06:56 -0300

    fix: layout shifting

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Dialog({ message }: Props) {
   const state = useViewState();
   return (
-    <div>
+    <div style={{height: "50px"}}>
       {state.status === GameStatus.Won && <h2>You Won</h2>}
       {state.status === GameStatus.Failed && <h2>You Lost</h2>}
       {message && <h2>{message}</h2>}
